fd92e63c990d9c0a1f41097aa1e686d0680bb460
fsduyx <anafievtimur007@gmail.com>
2025-10-20 11:04:50 +0300

    Initial commit

---
+++ buildSrc/src/main/kotlin/1lesson.kt
@@ -0,0 +1,62 @@
+import kotlin.random.Random
+
+fun main(){
+    println("Привет, это моя первая строка на котлин")
+    println("Добро пожаловать")
+
+    // var - изменяемая перемернная
+    // val - не изменяемая переменная
+    // :String - задание явного типа данных для переменной
+    var playerName: String = "Oleg"
+    val maxHealth: Int = 100
+
+    var currentHealth: Int = 100
+
+    var playerAttack: Int = 15
+
+    var isAlive: Boolean = true
+
+    var gold: Int = 50
+
+    println("Игрок $playerName входит в игру")
+    println("Здоровье: $currentHealth / $maxHealth")
+    println("Всего монет: $gold")
+    println("Он жив? Ответ: $isAlive")
+
+    println("\n >>> $playerName наступает на ловушку! <<<")
+    val trapDamage: Int = 35
+
+    // -= currentHealth = currentHealth - trapDamage
+    currentHealth -= trapDamage
+
+    if(currentHealth <= 0){
+        isAlive = false
+        println("Игрок $playerName помер в нищете")
+    }else{
+        println("$playerName пережил! у его осталось хп: $currentHealth")
+    }
+
+    val damageVariation = Random.nextInt(80,121)
+
+    val actualDamage = (playerAttack * damageVariation) / 100
+    val monsterName: String = "ТунТунТун Сагур"
+    var monsterHealth: Int = 50
+    val monsterAttack: Int = 12
+
+    monsterHealth -= actualDamage
+    println("$playerName наносит $monsterName $actualDamage урона")
+    if (monsterHealth <= 0){
+        println("$monsterName умер в нищете от руки $playerName")
+    }else{
+        println("$monsterName не умер, у него осталось $monsterHealth здоровья")
+    }
+
+    currentHealth -= monsterAttack
+
+    isAlive = currentHealth > 0
+    if (!isAlive){
+        println("$monsterName победил BRUH")
+    }else{
+        println("$playerName получает $monsterAttack урона! \n Осталось $currentHealth здоровья")
+    }
+}
