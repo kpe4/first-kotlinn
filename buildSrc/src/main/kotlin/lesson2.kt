03091a47f533ef47c7c61332bb508420e760fac2
fsduyx <anafievtimur007@gmail.com>
2025-10-21 08:35:55 +0300

    upd

---
+++ buildSrc/src/main/kotlin/lesson2.kt
@@ -0,0 +1,19 @@
+import kotlin.random.Random
+
+fun main(){
+    println("=== VOYNA S SATANOY ===")
+    val playerName: String = "Oleg"
+    var playerHealth: Int = 100
+    val playerAttack: Int = 18
+
+    val monsterName = "TunTunTun Sagurrr"
+    var monsterHealth = 60
+    val monsterAttack = 12
+
+    println("Player: $playerName (HP: $playerHealth, ATK: $playerAttack")
+    println("Monster: $monsterName (HP: $monsterHealth, ATK: $monsterAttack")
+    println("\n>>> FIGHT! <<<")
+
+    var round = 1
+    // C
+}
