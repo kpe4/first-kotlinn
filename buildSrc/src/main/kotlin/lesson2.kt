148934a77cbbab9df4922dd93cdfeb22201532c8
fsduyx <anafievtimur007@gmail.com>
2025-10-21 09:23:26 +0300

    battle added

--- buildSrc/src/main/kotlin/lesson2.kt
+++ buildSrc/src/main/kotlin/lesson2.kt
@@ -1,8 +1,23 @@
 import kotlin.random.Random
 
-fun main(){
-    println("=== VOYNA S SATANOY ===")
-    val playerName: String = "Oleg"
+fun calculateDamage(baseAttack: Int): Int{
+    val variation = Random.nextInt(80,121)
+    return (baseAttack * variation) / 100
+}
+
+fun performAttack(attackerName: String, attackerAttack: Int, defenderName: String, defenderHealth: Int):Int{
+    println("Move: $attackerName: ")
+    val damage = calculateDamage(attackerAttack)
+    var newHealth = defenderHealth - damage
+
+    println("$attackerName attack $defenderName with $damage damage")
+    println("$defenderName HP: $newHealth")
+
+    return newHealth
+}
+
+fun startBattle(){
+    val playerName: String = "Olegzon"
     var playerHealth: Int = 100
     val playerAttack: Int = 18
 
@@ -15,5 +30,32 @@ fun main(){
     println("\n>>> FIGHT! <<<")
 
     var round = 1
-    // C
+    // ЦИКЛ БОЯ ДО ПРОИГРАВШЕГО
+
+    // and - &&
+    // or - ||
+    while (playerHealth > 0 && monsterHealth > 0) {
+        println("\n--- Round: $round ---")
+
+        monsterHealth = performAttack(playerName, playerAttack, monsterName, monsterHealth)
+
+        if (monsterHealth <= 0) break // break - немедленный выход из циклa
+
+        // round++ то же самое что и round = round + 1
+        round++
+    }
+
+    println("\n=== Fight is over ===")
+
+    if (playerHealth > 0){
+        println("You win! $playerName beat $monsterName")
+    }else{
+        println("You lose! $monsterName beat $playerName")
+    }
+}
+
+fun main(){
+    println("=== VOYNA S SATANOY ===")
+    startBattle()
+    println("=== GAME IS OVER ===")
 }
