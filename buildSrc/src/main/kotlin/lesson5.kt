70c2f5289585b5c3effbc433ac4df3dcbeb41ee1
fsduyx <211399517+kpe4@users.noreply.github.com>
2025-11-11 09:42:31 +0300

    upd

---
+++ buildSrc/src/main/kotlin/lesson5.kt
@@ -0,0 +1,65 @@
+// ZADANIE 2
+class Character(
+    val characterName: String,
+    val characterLevel: Int
+){
+    fun checkLevel(){
+        if (characterLevel > 5){
+            println("$characterName - опытный Патапим!!!!")
+        }else{
+            println("$characterName - еще green(((")
+        }
+    }
+}
+
+// ZADANIE 4
+fun calculateExperience(level: Int): Int{
+    return level * 100
+}
+
+// NUMERO SINCO
+class Weapon(val name: String, val damage: Int){
+    fun displayInfo(){
+        println("$name (Урон: $damage)")
+    }
+}
+
+// ZADANIE 6
+open class Weapon(val name: String, val damage: Int):
+    open fun displayInfo(){
+        println("$name (урон: $damage)")
+    }
+}
+
+class MagicWeapon(name: String, damage: Int, val manaCost: Int): Weapon(name, damage){
+    override fun displayInfo(){
+        println("$name (урон: $damage, мана: $manaCost)")
+    }
+}
+
+// MAIN
+fun main(){
+    // 2
+    val hero1 = Character("Steve", 77)
+    val hero2 = Character("Alex", 2)
+
+    hero1.checkLevel()
+    hero2.checkLevel()
+
+    // 3
+    val level1 = 3
+    val level2 = 7
+
+    // 4
+    println("Для уровня $level1 нужно ${calculateExperience(level1)} опыта.")
+    println("Для уровня $level2 нужно ${calculateExperience(level2)} опыта.")
+
+    // 5
+    val sword = Weapon("Меч", 25)
+
+    sword.displayInfo()
+
+    // 6
+    val magicSword = MagicWeapon("Огненный Меч", 120, 50)
+    magicSword.displayInfo()
+}
